a8929420ae04dab9210f7ee748911a2d157009a1
Ying WANG <ying.wang@grafana.com>
2021-11-24 16:23:13 +0100

    update some unittest

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -1100,7 +1100,6 @@ func (cfg *Cfg) readSessionConfig() {
 }
 
 func (cfg *Cfg) initLogging(file *ini.File) error {
-	fmt.Print("I am in the initLogging")
 	logModeStr := valueAsString(file.Section("log"), "mode", "console")
 	// split on comma
 	logModes := strings.Split(logModeStr, ",")
